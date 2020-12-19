942687fbf28a515a3f2f5c74313497dbecc34ea0
ShedowSith <rahimyanov_aaleks@mail.ru>
2020-12-19 21:55:11 +0300

    Commit 9 Homework_3 Параметризовать CountMap и реализовать его.

---
+++ Module1/src/main/java/homework_3/CountMap.java
@@ -0,0 +1,39 @@
+package homework_3;
+import java.util.Map;
+
+public interface CountMap <T>{
+
+    // добавляет элемент в этот контейнер.
+    void add(T o);
+    //Возвращает количество добавлений данного элемента
+    int getCount(T o);
+    ///Удаляет элемент и контейнера и возвращает количество его добавлений(до удаления)
+    int remove(T o);
+    //количество разных элементов
+    int size();
+    //Добавить все элементы из source в текущий контейнер,
+    // при совпадении ключей, суммировать значения
+    void addAll(CountMap source);
+    //Вернуть java.util.Map. ключ - добавленный элемент,
+    // значение - количество его добавлений
+    Map<? super T, Integer> toMap();
+    //Тот же самый контракт как и toMap(), только всю информацию записать в destination
+    void toMap(Map<? super T, Integer > destination);
+
+    public static void main(String[] args) {
+        CountMap map = new CountMapImpl();
+        map.add(10);
+        map.add(10);
+        map.add(5);
+        map.add(6);
+        map.add(5);
+        map.add(10);
+
+        int count = map.getCount(5); // 2
+        System.out.println(count);
+        int count2 = map.getCount(6); // 1
+        System.out.println(count2);
+        int count3 = map.getCount(10); // 3
+        System.out.println(count3);
+    }
+}
---
+++ Module1/src/main/java/homework_3/CountMapImpl.java
@@ -0,0 +1,47 @@
+package homework_3;
+
+import java.util.*;
+
+public class CountMapImpl<T> implements CountMap<T>{
+    private Map<T, Integer> map = new HashMap<>();
+    @Override
+    public void add(T o) {
+        map.put(o, map.getOrDefault(o, 0)+1);
+    }
+
+    @Override
+    public int getCount(T o) {
+        return map.getOrDefault(o, 0);
+    }
+
+    @Override
+    public int remove(T o) {
+        int count = map.getOrDefault(o, 0);
+        map.remove(o);
+        return count;
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public void addAll(CountMap source) {
+        Iterator<Map.Entry<T, Integer>> iterator = source.toMap().entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<T, Integer> entry = iterator.next();
+            map.put(entry.getKey(), map.getOrDefault(entry.getKey(), entry.getValue())+entry.getValue());
+        }
+    }
+
+    @Override
+    public Map<? super T, Integer> toMap() {
+        return map;
+    }
+
+    @Override
+    public void toMap(Map<? super T, Integer> destination) {
+        map.forEach((k,v) -> destination.put(k,v));
+    }
+}
